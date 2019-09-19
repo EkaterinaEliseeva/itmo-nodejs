7237244ed503d44f9bce52239eeda91dc04254a7
EkaterinaEliseeva <katerina17778@gmail.com>
2019-09-19 14:58:17 +0300

    added scripts decrypt and encrypt

---
+++ secret/encrypt.js
@@ -0,0 +1,12 @@
+const crypto = require("crypto");
+const fs = require("fs");
+const message = `/*EliseevaES*/const cr=require("crypto"),fs=require("fs"),pK=fs.readFileSync("key"),m=Buffer.from(fs.readFileSync("secret")),dM=cr.publicDecrypt(pK,m).toString();console.log(dM);`;
+const publicKey = fs.readFileSync('key');
+const messageBuffer = Buffer.from(message);
+const encryptedMessage = crypto.publicEncrypt(publicKey, messageBuffer).toString();
+fs.writeFileSync("encrypt", encryptedMessage);
+
+console.log(encryptedMessage);
+// const decBuffer = Buffer.from(fs.readFileSync('encrypt'));
+// const decryptedMessage = crypto.publicDecrypt(publicKey, decBuffer).toString('utf8');
+// console.log(decryptedMessage);
