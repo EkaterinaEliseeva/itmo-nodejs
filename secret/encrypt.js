b03171959598ee18b6f53374a23e39caac7a708f
EkaterinaEliseeva <katerina17778@gmail.com>
2019-09-19 19:51:28 +0300

    changes files

--- secret/encrypt.js
+++ secret/encrypt.js
@@ -1,6 +1,8 @@
 const crypto = require("crypto");
 const fs = require("fs");
-const message = `/*EliseevaES*/const cr=require("crypto"),fs=require("fs"),pK=fs.readFileSync("key"),m=Buffer.from(fs.readFileSync("secret")),dM=cr.publicDecrypt(pK,m).toString();console.log(dM);`;
+//const message = `/*EliseevaES*/const cr=require("crypto"),fs=require("fs"),pK=fs.readFileSync("key"),m=Buffer.from(fs.readFileSync("secret")),dM=cr.publicDecrypt(pK,m).toString();console.log(dM);`;
+const message = fs.readFileSync('index.js');
+console.log(message);
 const publicKey = fs.readFileSync('key');
 const messageBuffer = Buffer.from(message);
 const encryptedMessage = crypto.publicEncrypt(publicKey, messageBuffer).toString();
