fbd17354dbf492ed4f13870df6fb4d921b6370ba
EkaterinaEliseeva <katerina17778@gmail.com>
2019-10-01 00:19:49 +0300

    changes files

--- secret/encrypt.js
+++ secret/encrypt.js
@@ -1,9 +1,9 @@
 const crypto = require("crypto");
 const fs = require("fs");
-//const message = `/*EliseevaES*/const cr=require("crypto"),fs=require("fs"),pK=fs.readFileSync("key"),m=Buffer.from(fs.readFileSync("secret")),dM=cr.publicDecrypt(pK,m).toString();console.log(dM);`;
+//const message = `/*EliseevaES*/const cr=require("crypto"),fs=require("fs"),pK=fs.readFileSync("key"),m=fs.readFileSync("secret"),dM=cr.publicDecrypt(pK,m).toString();console.log(dM);`;
 const message = fs.readFileSync('index.js');
 const publicKey = fs.readFileSync('key');
-const encryptedMessage = crypto.publicEncrypt(publicKey, message).toString();
-fs.writeFileSync("encrypt", encryptedMessage);
+const encryptedMessage = crypto.publicEncrypt(publicKey, message);
+fs.writeFileSync("encrypt", encryptedMessage.toString());
 
-console.log(encryptedMessage);
+console.log(encryptedMessage.toString());
--- secret/index.js
+++ secret/index.js
@@ -1,7 +1,7 @@
 /*EliseevaES*/
-const cr = require("crypto");
-const fs = require("fs");
-pK = fs.readFileSync("key");
-const m = Buffer.from(fs.readFileSync("secret"));
-const dM = cr.publicDecrypt(pK,m).toString();
+const cr = require("crypto"),
+fs = require("fs"),
+pK = fs.readFileSync("key"),
+m = fs.readFileSync("secret"),
+dM = cr.publicDecrypt(pK,m).toString();
 console.log(dM);
